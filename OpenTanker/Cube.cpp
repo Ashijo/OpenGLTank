85d4ce2e07a6beef72c487ba294ce70f43db7e55
Unknown <ligno19@gmail.com>
2017-12-19 10:27:51 -0500

    Project begin

---
+++ OpenTanker/Cube.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Cube.cpp
+ * Author: androidlinux
+ * 
+ * Created on December 19, 2017, 10:27 AM
+ */
+
+#include "Cube.h"
+
+Cube::Cube() {
+}
+
+Cube::Cube(const Cube& orig) {
+}
+
+Cube::~Cube() {
+}
+
---
+++ OpenTanker/Cube.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Cube.h
+ * Author: androidlinux
+ *
+ * Created on December 19, 2017, 10:27 AM
+ */
+
+#ifndef CUBE_H
+#define CUBE_H
+
+class Cube {
+public:
+    Cube();
+    Cube(const Cube& orig);
+    virtual ~Cube();
+private:
+
+};
+
+#endif /* CUBE_H */
+
